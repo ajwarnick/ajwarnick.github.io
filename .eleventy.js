c2ca38e8f5909782272b3c76719dc1fe01fec477
Anthony Warnick <ajwarnick@gmail.com>
2019-12-01 20:34:50 -0600

    update 11ty file to add .nojekyll

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('.nojekyll');
 
 
     eleventyConfig.addCollection("everything", function(collection) {
