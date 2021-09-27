68f522867483982c25674a29d74989ae735270d3
Anthony Warnick <ajwarnick@gmail.com>
2021-09-27 18:10:25 -0500

    Update: dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,54 @@
+const Image = require("@11ty/eleventy-img");
+
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
-    eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('js');
     eleventyConfig.addPassthroughCopy('.nojekyll');
+
+    eleventyConfig.addWatchTarget("./_src/sass/");
+
+    eleventyConfig.addNunjucksAsyncShortcode("ResponsiveImage", async (src, alt) => {
+        if (!alt) {
+          throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+        }
+    
+        // let stats = await Image(src, {
+        //   widths: [25, 320, 640, 960, 1200, 1800, 2400],
+        //   formats: ["jpeg", "webp"],
+        //   urlPath: "/img/",
+        //   outputDir: "./_site/img/responsive/",
+        // });
+
+        return "testss"
+    
+        // let lowestSrc = stats["jpeg"][0];
+    
+        // const srcset = Object.keys(stats).reduce(
+        //   (acc, format) => ({
+        //     ...acc,
+        //     [format]: stats[format].reduce(
+        //       (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+        //       ""
+        //     ),
+        //   }),
+        //   {}
+        // );
+    
+        // const source = `<source type="image/webp" srcset="${srcset["webp"]}" >`;
+    
+        // const img = `<img
+        //   loading="lazy"
+        //   alt="${alt}"
+        //   src="${lowestSrc.url}"
+        //   sizes='(min-width: 1024px) 1024px, 100vw'
+        //   srcset="${srcset["jpeg"]}"
+        //   width="${lowestSrc.width}"
+        //   height="${lowestSrc.height}">`;
+    
+        // return `<div class="image-wrapper"><picture> ${source} ${img} </picture></div>`;
+      });
+
 
 
     eleventyConfig.addCollection("everything", function(collection) {
